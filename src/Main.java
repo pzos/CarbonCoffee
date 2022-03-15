902fd6f5c29d370927d0eaa59dbe43f7d2ec0847
HackerDaGreat57 <contact.akshatsingh@gmail.com>
2022-03-14 22:14:21 -0700

    Will resume work tomorrow

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to CarbonCoffee!");
+public class CC_Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      /*
+      Code for JavaFX application.
+      (Stage, scene, scene graph)
+      */
+    }
+    public static void main(String args[]){
+        launch(args);
     }
 }
