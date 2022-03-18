add297c2368fd13da660b40ef29b029829dd1b9a
HackerDaGreat57 <contact.akshatsingh@gmail.com>
2022-03-17 20:57:51 -0700

    Taking a break for a while

--- src/CC_A.java
+++ src/CC_A.java
@@ -1,14 +1,31 @@
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.*;
+import javafx.stage.*;
+import javafx.scene.paint.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class CC_A extends Application {
     @Override
-    public void start(Stage CC_S_Main) {
+    public void start(Stage CC_S_Main) throws FileNotFoundException {
+        //Font CC_F_Header1 = new Font();
+        Image CC_I_suitcase_general = new Image(new FileInputStream("data_DONT_TOUCH_PLEASE/rsrc/suitcase_general.png"));
+        ImageView CC_IMGV_suitcase_general = new ImageView(CC_I_suitcase_general);
         Label CC_CTRL_Label_Title = new Label("CarbonCoffee");
-        Scene CC_SC_Main = new Scene(new StackPane(CC_CTRL_Label_Title), 640, 480);
+        Group CC_GR_Main_Controls = new Group(CC_CTRL_Label_Title);
+        Scene CC_SC_Main = new Scene(CC_GR_Main_Controls, 794, 663);
+        Stop[] CC_G_Main_Background = new Stop[] {
+                new Stop(0, Color.rgb(75, 75, 75, 1)),
+                new Stop(1, Color.rgb(80, 80, 80, 1))
+        };
+        LinearGradient CC_G_L_Main_Background = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, CC_G_Main_Background);
+        CC_SC_Main.setFill(CC_G_L_Main_Background);
+
         CC_S_Main.setScene(CC_SC_Main);
         CC_S_Main.show();
     }
@@ -16,5 +33,4 @@ public class CC_A extends Application {
     public static void Main(String[] args) {
         launch();
     }
-
 }
